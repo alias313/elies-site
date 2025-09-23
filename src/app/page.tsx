b343804f4197f3c45403d78f38ac448187d13bf2
Elies <68759092+eliesgalvira@users.noreply.github.com>
2025-09-24 00:46:49 +0200

    Fix preload error
    The error is 'The resource was preloaded using link preload but not used within a few seconds from the window's load event.'

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,7 +26,6 @@ export default function Home() {
                   fill
                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                   className="h-full w-full rounded-lg object-cover"
-                  priority
                 />
               </AspectRatio>
             </div>
